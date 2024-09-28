2534db1c346b55ace44ab9a6a234e7707e07de5f
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2024-09-28 11:33:47 +0200

    Implement next|prev as commands to sent/receive
    Implement commands which are on startup send to the other instance.
    Currently we support:
    - next: Advance the displayed month to the next month
    - prev: Same as next but switch to previous month

--- include/calfenster/app_server.h
+++ include/calfenster/app_server.h
@@ -1,21 +1,30 @@
 #pragma once
 
+#include <qglobal.h>
 #include <qobject.h>
 
 #include <QLocalServer>
-
-// #include "app_server.moc"
+#include <optional>
 
 namespace calfenster {
 class AppServer : public QObject {
   Q_OBJECT
 
  public:
+  enum class Command : quint8 {
+    kShutdown = 1,
+    kPrevMonth,
+    kNextMonth,
+    kCommandSize  // keep as last for sanity check
+  };
+
   explicit AppServer(QObject* parent = nullptr);
   virtual ~AppServer() = default;
 
   [[nodiscard]] bool HasOtherInstance() const { return other_instance_; }
-  bool SendShutdownRequest();
+  bool SendCommand(Command cmd);
+
+  static std::optional<Command> CommandFromString(const QString& command);
 
  protected:
   QLocalServer* server_ = nullptr;
@@ -24,5 +33,9 @@ class AppServer : public QObject {
  public slots:
   void SlotNewConnection();
   void SlotReadClient();
+
+ signals:
+  void ShowPrevMonth();
+  void ShowNextMonth();
 };
 }  // namespace calfenster
