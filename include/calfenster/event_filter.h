50308e76f4f88e5e7e495dc9e5303699d249313c
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2024-09-28 09:52:20 +0200

    Format files correctly

--- include/calfenster/event_filter.h
+++ include/calfenster/event_filter.h
@@ -7,17 +7,16 @@
 
 namespace calfenster {
 
-class EventFilter : public QObject
-{
+class EventFilter : public QObject {
   Q_OBJECT
-  public:
+ public:
   explicit EventFilter(QObject* parent = nullptr);
 
   QWidget* main_widget = nullptr;
   QWidget* calendar_widget = nullptr;
 
-protected:
-    bool eventFilter(QObject *obj, QEvent *event) override;
+ protected:
+  bool eventFilter(QObject* obj, QEvent* event) override;
 };
 
-}
+}  // namespace calfenster
